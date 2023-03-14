79aeb9bf4967eb1e80ed9108fbbd13676f59875d
Khoa-Nguyen1234 <nguyenkhoa020401@gmail.com>
2023-03-14 22:06:26 +0700

    update

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer Section Starts -->
+<footer class="footer">
+            <div class="wrapper">
+                <p class="text-center">2023 Pizza order - <a href="#">Khoa and Toan</a></p>
+            </div>
+        </footer>
+        <!-- Footer Section Ends -->
+    </body>
+</html>
