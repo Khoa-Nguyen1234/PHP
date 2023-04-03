cc57705ae31b828f7cc1f4fbcf0f8579d5df6f04
Khoa-Nguyen1234 <nguyenkhoa020401@gmail.com>
2023-04-04 00:19:52 +0700

    add ảnh xong (food)

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -6,48 +6,100 @@
 
                 <br /><br />
 
+
                 <!-- Button to Add Admin -->
-                <a href="#" class="btn-primary">Add Food</a>
+                <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
 
                 <br /> <br /> <br />
+
+                <?php
+                    if(isset($_SESSION['add']))
+                    {
+                        echo $_SESSION['add'];
+                        unset($_SESSION['add']);
+                    }
+                ?>
+
+                <br><br>
 
                 <table class="tbl-full">
                     <tr>
                         <th>S.N.</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                        <th>Image</th>
+                        <th>Featured</th>
+                        <th>Active</th>
                         <th>Actions</th>
+
                     </tr>
 
-                    <tr>
-                        <td>1. </td>
-                        <td>Minh Khoa</td>
-                        <td>Khoa</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                    <?php
+                        //Create a sql query  to get all the food
+                        $sql = "SELECT * FROM tbl_food";
 
-                    <tr>
-                        <td>2. </td>
-                        <td>Toan Nguyen</td>
-                        <td>Toan</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                        //Execute the query
+                        $res = mysqli_query($conn,$sql);
 
-                    <tr>
-                        <td>3. </td>
-                        <td>Son Goku</td>
-                        <td>Goku</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                        //Count rows to check whether we have foods or not
+                        $count = mysqli_num_rows($res);
+
+                        //Create serial number variable and set default value as 1
+                        $sn=1;
+
+                        if($count>0)
+                        {
+                            //We have food in database
+                            //Get the foods from database and display 
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                //get the values from individual columns
+                                $id = $row['id'];
+                                $title = $row['title'];
+                                $price = $row['price'];
+                                $image_name = $row['image_name'];
+                                $featured = $row['featured'];
+                                $active = $row['active'];
+                                ?>
+                                    <tr>
+                                        <td><?php echo $sn++; ?>. </td>
+                                        <td><?php echo $title; ?></td>
+                                        <td><?php echo $price; ?></td>
+                                        <td>
+                                            <?php 
+                                                //Check whether we have images or not
+                                                if($image_name=="")
+                                                {
+                                                    //We do not have images, display error message
+                                                    echo "<div class='error'>Image not added.</div>";
+                                                }
+                                                else
+                                                {
+                                                    //We have images, display image
+                                                    ?>
+                                                        <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px">
+                                                    <?php
+                                                }
+                                            ?>
+                                        </td>
+                                        <td><?php echo $featured; ?></td>
+                                        <td><?php echo $active; ?></td>
+                                        <td>
+                                            <a href="#" class="btn-secondary">Update Admin</a>
+                                            <a href="#" class="btn-danger">Delete Admin</a>
+                                        </td>
+                                    </tr>
+                                <?php
+                            }
+                        }
+                        else
+                        {
+                            //Food not added in database
+                            echo "<tr> <td colspan='7' class='error'> Food not added yet. </td> </tr>";
+                        }
+                    ?>
+
                 </table>
 
         </div>
